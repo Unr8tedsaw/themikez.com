899aeccf4e47e74c911c15aed28a95b5cdcbe941
Mike Z <findmikez@gmail.com>
2013-04-02 12:41:36 -0500

    Updated footer

--- wp-content/themes/twentyeleven/footer.php
+++ wp-content/themes/twentyeleven/footer.php
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the id=main div and all content after
- *
- * @package WordPress
- * @subpackage Twenty_Eleven
- * @since Twenty Eleven 1.0
- */
-?>
-
-	</div><!-- #main -->
-
-	<footer id="colophon" role="contentinfo">
-
-			<?php
-				/* A sidebar in the footer? Yep. You can can customize
-				 * your footer with three columns of widgets.
-				 */
-				if ( ! is_404() )
-					get_sidebar( 'footer' );
-			?>
-
-			<div id="site-generator">
-				<?php do_action( 'twentyeleven_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
-			</div>
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
-<?php wp_footer(); ?>
-
-</body>
-</html>
-=======
 <?php
 /**
  * The template for displaying the footer.
@@ -69,4 +32,3 @@
 
 </body>
 </html>
->>>>>>> 897cc59c4c5bca59a504e6d85b3d9f6306e65e99
